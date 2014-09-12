d8a2f6d17872e38cfcd775a84e55d10a7400c669
Andrea Cussolotto <Andrea_pietro@hotmail.it>
2014-09-12 20:05:43 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Corte Salandini management webapp</title>
+</head>
+
+<body>
+  <h1>This friendly php will help you!</h1>
+  <p><?php echo "Hello Mond!";?></p>
+</body>
+
+</html>
