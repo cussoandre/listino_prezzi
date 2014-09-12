81e53d3696c55386bae8f86baca1502724dbe8f4
Andrea Cussolotto <andrea_pietro@hotmail.it>
2014-09-12 18:47:02 +0000

    Added table displaying

--- index.php
+++ index.php
@@ -19,6 +19,41 @@
     {
       echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
+    // Check connection
+    if (mysqli_connect_errno()) 
+    {
+      echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
+    $result = mysqli_query($con,"SELECT * FROM Persons");
+    
+    echo "<table border='1'>
+    <tr>
+    
+      <th>Categoria</th>
+      <th>Sezione</th>
+      <th>Prodotto</th>
+      <th>Prezzo</th>
+      <th>Disponibilità</th>
+    
+    </tr>";
+    
+    while($row = mysqli_fetch_array($result)) 
+    {
+      echo "<tr>";
+      
+      echo "<td>" . $row['category'] . "</td>";
+      echo "<td>" . $row['section'] . "</td>";
+      echo "<td>" . $row['product'] . "</td>";
+      echo "<td>" . $row['price'] . "</td>";
+      echo "<td>" . $row['available'] . "</td>";
+      
+      echo "</tr>";
+    }
+    
+    echo "</table>";
+    
+    mysqli_close($con);
   ?>
 
   <footer>
