5bd4e74802d5c3708ae0dee97f06468c02d05491
Andrea Cussolotto <andrea_pietro@hotmail.it>
2014-09-13 11:33:40 +0000

    COMMIT

--- index.php
+++ index.php
@@ -45,6 +45,28 @@
     
     echo "</table>";
     
+    request = $.ajax({
+    url: "/redirect.php",
+    type: "post",
+    data: 'facebook'
+    });
+
+// callback handler that will be called on success
+request.done(function (response, textStatus, jqXHR){
+    // log a message to the console
+    console.log("Hooray, it worked!");
+});
+
+// callback handler that will be called on failure
+request.fail(function (jqXHR, textStatus, errorThrown){
+    // log the error to the console
+    console.error(
+        "The following error occured: "+
+        textStatus, errorThrown
+    );
+    });
+});
+    
     mysqli_close($con);
   ?>
 
