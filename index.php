720eea4cfb1be3ec134428fc126a1084b5a742ec
Andrea Cussolotto <andrea_pietro@hotmail.it>
2014-09-13 15:52:59 +0000

    COMMITmomo

--- index.php
+++ index.php
@@ -38,34 +38,12 @@
     {
       echo "<tr>";
       
-      echo '<td> <a href="/' . $row['category'] . '">' . $row['category'] . '</td>';
+      echo '<td> <a href="'. $_SERVER["PHP_SELF"] . '?sub=' . $row['category'] . '">' . $row['category'] . '</td>';
       
       echo "</tr>";
     }
     
     echo "</table>";
-    
-    request = $.ajax({
-    url: "/redirect.php",
-    type: "post",
-    data: 'facebook'
-    });
-
-// callback handler that will be called on success
-request.done(function (response, textStatus, jqXHR){
-    // log a message to the console
-    console.log("Hooray, it worked!");
-});
-
-// callback handler that will be called on failure
-request.fail(function (jqXHR, textStatus, errorThrown){
-    // log the error to the console
-    console.error(
-        "The following error occured: "+
-        textStatus, errorThrown
-    );
-    });
-});
     
     mysqli_close($con);
   ?>
